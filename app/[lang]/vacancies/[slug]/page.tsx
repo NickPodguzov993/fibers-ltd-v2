2aa56a2dd595dff22902b255bd82eb45554c91a8
Роман Саликов <rsalikov@it-fintech.com>
2023-10-19 10:10:23 +0300

    feat: desktop vacancies detail nav

--- app/[lang]/vacancies/[slug]/page.tsx
+++ app/[lang]/vacancies/[slug]/page.tsx
@@ -1,8 +1,10 @@
 import clsx from "clsx";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { PropsWithLanguage, getDictionary } from "@/lib/i18n";
 import { titleFont } from "@/lib/fonts";
 import { PrimaryButton } from "@/components/shared/buttons";
+import Image from "next/image";
 
 export async function generateStaticParams({
   params: { lang },
@@ -58,11 +60,43 @@ export default async function Vacancy({
           </div>
         </div>
       </div>
-      <div className="lg:fixed lg:w-[496px] flex-1 lg:right-10 p-4 h-fit flex flex-col gap-4 rounded bg-light-violet text-center">
-        <p className="lg:text-[18px] font-bold leading-paragraph text-dark">
-          Did we miss something? Write to us
-        </p>
-        <PrimaryButton>Contact us</PrimaryButton>
+      <div className="lg:fixed lg:w-[496px] flex-1 lg:right-10 h-fit flex flex-col rounded bg-light-violet">
+        <div className="hidden lg:flex flex-col">
+          <h2
+            className={clsx(
+              "p-8 text-[24px] font-bold text-dark",
+              titleFont.className
+            )}
+          >
+            Other vacancies
+          </h2>
+          <div className="flex flex-col px-2">
+            {vacancies
+              .filter((v) => v.slug !== slug)
+              .map(({ title, slug }) => (
+                <Link
+                  key={title}
+                  href={`/${lang}/vacancies/${slug}`}
+                  className="group flex gap-4 justify-between items-center px-8 py-4 text-[18px] font-bold leading-paragraph text-dark rounded-xs transition-all border-2 border-[transparent] duration-300 hover:bg-white hover:border-violet hover:shadow-header active:bg-white active:border-violet active:shadow-header"
+                >
+                  <h3>{title}</h3>
+                  <Image
+                    src="/icons/open-in-new.svg"
+                    alt=""
+                    className="opacity-0 group-hover:opacity-100 font-normal text-sm transition-opacity duration-300"
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+              ))}
+          </div>
+        </div>
+        <div className="p-4 flex flex-col gap-4 text-center">
+          <p className="lg:text-[18px] font-bold leading-paragraph text-dark">
+            Did we miss something? Write to us
+          </p>
+          <PrimaryButton>Contact us</PrimaryButton>
+        </div>
       </div>
     </div>
   );
